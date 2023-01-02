73f2b75ecf235df53cf10cd7703ce1082f846ac3
Synqat <ken@slinky.link>
2023-01-02 13:28:22 -0700

    init.sh

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,14 +1,39 @@
-# Prompt
-Import-Module posh-git
-Import-Module oh-my-posh
+function CheckIfInstalled($module_name) {
+    try {
+        Import-Module $module_name -ErrorAction Stop
+        Write-Host "Module $module_name is installed" -ForegroundColor Green
+    } catch {
+        Write-Host "Module $module_name is not installed" -ForegroundColor Red
+    }
+}
+
+function CheckIfCommandExists($command_name) {
+    try {
+        Get-Command $command_name -ErrorAction Stop | Out-Null
+        Write-Host "Module $command_name is installed" -ForegroundColor Green
+    } catch {
+        Write-Host "Module $command_name is not installed" -ForegroundColor Red
+    }
+}
+
+function SetEnvPathForCommand($command_name, $path) {
+    $env:Path = $env:Path + ";" + $path
+    Write-Host "Added $path to PATH" -ForegroundColor Green
+}
+
+SetEnvPathForCommand "nvim" "C:\Program Files\Neovim\bin"
+SetEnvPathForCommand "fzf" "$env:USERPROFILE\.fzf"
+
+CheckIfCommandExists "oh-my-posh"
+CheckIfCommandExists "nvim"
+CheckIfInstalled "posh-git"
+CheckIfInstalled "Terminal-Icons"
+CheckIfInstalled "PSFzf"
 
 # Load prompt config
-function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
-$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'omp.json'
+$PROMPT_CONFIG = Join-Path './' 'powershell' 'omp.json'
 oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
-
-# Icons
-Import-Module -Name Terminal-Icons
+Write-Host "oh-my-posh config loaded." -ForegroundColor Green
 
 # PSReadLine
 Set-PSReadLineOption -EditMode Emacs
@@ -16,13 +41,15 @@ Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
+Write-Host "PSReadLine config loaded." -ForegroundColor Green
 
 # Fzf
-Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+Write-Host "PSFzf config loaded." -ForegroundColor Green
 
 # Alias
 Set-Alias y yarn
+Set-Alias yg 'yarn global'
 Set-Alias rm rimraf
 Set-Alias vim nvim
 Set-Alias ll ls
@@ -30,9 +57,11 @@ Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Write-Host "Alias config loaded." -ForegroundColor Green
 
 # Utilities
 function gimme ($command) {
     Get-Command -Name $command -ErrorAction SilentlyContinue |
         Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
+Write-Host "Command 'gimme' loaded." -ForegroundColor Green
