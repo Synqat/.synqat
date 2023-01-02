a256ddf6a8122e30c75f6dfddb8367dfad2c6b20
Synqat <ken@slinky.link>
2023-01-02 13:41:56 -0700

    fix: profile refresh & profile silence

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,11 +1,11 @@
-$IS_RUNNING_SILENT = $args -contains "-SilenceProfile"
-
-echo $IS_RUNNING_SILENT
+$IS_RUNNING_SILENT = $env:SILENCE_PROFILE -eq "true"
 
 function CheckIfInstalled($module_name) {
     try {
         Import-Module $module_name -ErrorAction Stop
-        Write-Host "Module $module_name is installed" -ForegroundColor Green
+        if (-not $IS_RUNNING_SILENT) {
+            Write-Host "Module $module_name is installed" -ForegroundColor Green
+        }
     } catch {
         Write-Host "Module $module_name is not installed" -ForegroundColor Red
     }
@@ -14,7 +14,9 @@ function CheckIfInstalled($module_name) {
 function CheckIfCommandExists($command_name) {
     try {
         Get-Command $command_name -ErrorAction Stop | Out-Null
-        Write-Host "Module $command_name is installed" -ForegroundColor Green
+        if (-not $IS_RUNNING_SILENT) {
+            Write-Host "Module $command_name is installed" -ForegroundColor Green
+        }
     } catch {
         Write-Host "Module $command_name is not installed" -ForegroundColor Red
     }
@@ -22,7 +24,9 @@ function CheckIfCommandExists($command_name) {
 
 function SetEnvPathForCommand($command_name, $path) {
     $env:Path = $env:Path + ";" + $path
-    Write-Host "Added $path to PATH" -ForegroundColor Green
+    if (-not $IS_RUNNING_SILENT) {
+        Write-Host "Added $path to PATH" -ForegroundColor Green
+    }
 }
 
 SetEnvPathForCommand "nvim" "C:\Program Files\Neovim\bin"
@@ -35,9 +39,11 @@ CheckIfInstalled "Terminal-Icons"
 CheckIfInstalled "PSFzf"
 
 # Load prompt config
-$PROMPT_CONFIG = Join-Path './' 'powershell' 'omp.json'
+$PROMPT_CONFIG = 'https://raw.githubusercontent.com/Synqat/.config/main/powershell/omp.json'
 oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
-Write-Host "oh-my-posh config loaded." -ForegroundColor Green
+if (-not $IS_RUNNING_SILENT) {
+    Write-Host "oh-my-posh config loaded." -ForegroundColor Green
+}
 
 # PSReadLine
 Set-PSReadLineOption -EditMode Emacs
@@ -45,11 +51,15 @@ Set-PSReadLineOption -BellStyle None
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
-Write-Host "PSReadLine config loaded." -ForegroundColor Green
+if (-not $IS_RUNNING_SILENT) {
+    Write-Host "PSReadLine config loaded." -ForegroundColor Green
+}
 
 # Fzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-Write-Host "PSFzf config loaded." -ForegroundColor Green
+if (-not $IS_RUNNING_SILENT) {
+    Write-Host "PSFzf config loaded." -ForegroundColor Green
+}
 
 # Alias
 Set-Alias y yarn
@@ -61,11 +71,15 @@ Set-Alias g git
 Set-Alias grep findstr
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-Write-Host "Alias config loaded." -ForegroundColor Green
+if (-not $IS_RUNNING_SILENT) {
+    Write-Host "Alias config loaded." -ForegroundColor Green
+}
 
 # Utilities
 function gimme ($command) {
     Get-Command -Name $command -ErrorAction SilentlyContinue |
         Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
-Write-Host "Command 'gimme' loaded." -ForegroundColor Green
+if (-not $IS_RUNNING_SILENT) {
+    Write-Host "Command 'gimme' loaded." -ForegroundColor Green
+}
--- refresh-profile.ps1
+++ refresh-profile.ps1
@@ -1,7 +1,9 @@
 $PROMPT_CONFIG = Join-Path './' 'powershell' 'omp.json'
 $PROMPT_PROFILE = Join-Path './' 'powershell' 'profile.ps1'
 
-Write-Host "\n\nRefreshing PowerShell profile..." -ForegroundColor Yellow
+Write-Host ""
+Write-Host ""
+Write-Host "Refreshing PowerShell profile..." -ForegroundColor Yellow
 
 if (!(Test-Path -Path $PROFILE)) {
     New-Item -Path $PROFILE -ItemType File -Force
@@ -10,6 +12,8 @@ if (!(Test-Path -Path $PROFILE)) {
 Set-Content -Path $PROFILE -Value $(Get-Content -Path $PROMPT_PROFILE)
 
 Write-Host "PowerShell profile refreshed" -ForegroundColor Green
+Write-Host ""
+Write-Host ""
 
 pwsh.exe
 
